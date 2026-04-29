512d1fca29e79320625cc82a6d582d993e0f4815
Luka <luka.matshebelele@gmail.com>
2026-04-29 19:24:56 +0200

    refactor fetchWithAuth

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -7,19 +7,69 @@ function Dashboard() {
 
     const { isLoaded, isSignedIn, userId, sessionId, getToken, signOut } = useAuth();
 
-    useEffect(() => {
 
-        const verifyToken = async () => {
-            const token = await getToken();
+
+    const fetchWithAuth = async (url: string) => {
+
+        try {
+
+            if (!isLoaded || !isSignedIn) {
+                throw new Error("Auth not ready");
+            }
+
+            const token = await getToken({ template: "backend" });
 
             if (!token) {
-                console.error("No token found, user is not authenticated.");
+                throw new Error("No token found");
+            }
+
+            const user = await fetch(url, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            });
+
+            if (!user.ok) {
+                throw new Error("Failed to fetch user data");
+            }
+
+            const data = await user.json();
+
+            return data;
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+            // throw new Error("Failed to fetch user data");
+        }
+    }
+
+    useEffect(() => {
+
+        if (!isLoaded) return;
+
+        if (!isSignedIn) {
+            navigate("/sign-in");
+            return;
+        }
+
+        let isMounted = true;
+
+        const loadUserData = async () => {
+
+            try {
+
+                const data = await fetchWithAuth("http://localhost:3000/api/user");
+
+                if (!isMounted) return;
+
+                console.log("User data..:", data);
+                return data;
+            } catch (error) {
+                console.error("Error loading user data:", error);
                 navigate("/sign-in");
-                return;
             }
         }
 
-        verifyToken();
+        loadUserData();
     }, [isLoaded, isSignedIn, getToken, navigate]);
 
     const logout = () => {
