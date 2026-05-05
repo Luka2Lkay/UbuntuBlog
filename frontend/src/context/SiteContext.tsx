d5a00966e8d2fc8d829f6787fa9f32adfdbd5072
Luka <luka.matshebelele@gmail.com>
2026-05-05 18:12:06 +0200

    create a siteContext

---
+++ frontend/src/context/SiteContext.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+
+type ReactNode = React.ReactNode;
+type SiteContextType = {
+    site: string;
+    setSite: React.Dispatch<React.SetStateAction<string>>;
+}
+type SiteProviderProps = {
+    children: ReactNode;
+}
+
+export const SiteContext = createContext<SiteContextType | undefined>(undefined);
+
+export function SiteProvider({ children }: SiteProviderProps) {
+    const [site, setSite] = useState("Home of Commerce");
+
+
+
+    return (
+        <SiteContext.Provider value={{ site, setSite }}>
+            {children}
+        </SiteContext.Provider>
+    )
+}
