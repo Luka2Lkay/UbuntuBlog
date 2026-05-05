5c37de384bb676ad04f5b130941cd438dfe43e7b
Luka <luka.matshebelele@gmail.com>
2026-05-05 16:50:28 +0200

    add services

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "@clerk/react"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import axios from "axios";
+import { fetchWithAuth } from "../../services/api"
 
 const base_url = import.meta.env.BASE_URL || "http://localhost:3000";
 
@@ -10,36 +10,7 @@ function Dashboard() {
 
     const { isLoaded, isSignedIn, userId, sessionId, getToken, signOut } = useAuth();
 
-    const fetchWithAuth = async (url: string) => {
 
-        try {
-
-            if (!isLoaded || !isSignedIn) {
-                throw new Error("Auth not ready");
-            }
-
-            const token = await getToken({ template: "backend" });
-
-            if (!token) {
-                throw new Error("No token found");
-            }
-
-            const response = await axios.get(url, {
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "Content-Type": "application/json"
-                },
-            });
-
-            const data = response.data;
-
-            console.log("Fetched user data:", data);
-            return data;
-        } catch (error) {
-            console.error("Error fetching user data:", error);
-            throw new Error("Failed to fetch user data");
-        }
-    }
 
     useEffect(() => {
 
@@ -53,7 +24,7 @@ function Dashboard() {
         const loadUserData = async () => {
 
             try {
-                const data = await fetchWithAuth(`${base_url}/api/user`);
+                const data = await fetchWithAuth(`${base_url}/api/user`, getToken);
                 return data;
             } catch (error) {
                 console.error("Error loading user data:", error);
@@ -77,7 +48,7 @@ function Dashboard() {
                         <h1> Hello, {userId}! Your current active session is {sessionId}.</h1>
 
                         <div className="flex flex-col gap-4 mt-4">
-                            <button className="text-green-500" onClick={() => fetchWithAuth("http://localhost:3000/api/user")}>Fetch User Data</button>
+                            <button className="text-green-500" onClick={() => fetchWithAuth("http://localhost:3000/api/user", getToken)}>Fetch User Data</button>
                             <button onClick={logout}>Sign Out</button>
                         </div>
 
