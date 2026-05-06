2b5fced996ffff8276f1e568b5dc177bd8d0317a
Luka <luka.matshebelele@gmail.com>
2026-05-06 19:19:34 +0200

    dashboard skeleton

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
 import { useAuth } from "@clerk/react"
 import { useEffect, } from "react"
-import { useNavigate, Outlet } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { fetchWithAuth } from "../../services/api"
 import { useSiteContext } from "../../context/SiteContext"
+import StatisticsCard from "../../components/statistics_card/StatisticsCard"
+import { Link } from "react-router-dom"
 
 const base_url = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 
@@ -42,7 +44,27 @@ function Dashboard() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <StatisticsCard title="Total Posts" value={5} />
+                <StatisticsCard title="Published" value={2} />
+                <StatisticsCard title="Drafts" value={3} />
+            </div>
 
+            <div className="flex gap-3">
+                <Link to="/create-post" className="px-4 py-2 bg-gray-900 text-white rounded-md text-sm hover:bg-black">
+                    + New Post
+                </Link>
+
+                <Link to="/posts" className="px-4 py-2 border border-gray-300 rounded-md text-sm hover:bg-gray-200">
+                    View All Posts
+                </Link>
+            </div>
+
+            <div className="bg-white rounded-lg border border-gray-200">
+                <div className="px-4 py-3 border-b border-gray-200">
+                    <h2 className="text-sm font-medium text-gray-700">Recent Posts</h2>
+                </div>
+
+                
             </div>
         </div>
     )
