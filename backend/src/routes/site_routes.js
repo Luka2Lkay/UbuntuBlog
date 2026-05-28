d4039a3b41687267550b9d692f0d9d81acba5efe
Luka <luka.matshebelele@gmail.com>
2026-05-28 14:56:58 +0200

    add get sites router

--- backend/src/routes/site_routes.js
+++ backend/src/routes/site_routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { createSite } = require("../controllers/site_controller");
+const { createSite, getSites } = require("../controllers/site_controller");
 const { validateSiteCreation } = require("../middleware/validation");
 
 const siteRoutes = (app) => {
   const router = express.Router();
 
   router.post("/sites", validateSiteCreation, createSite);
+  router.get("/sites", getSites)
 
   app.use("/api", router);
 };
