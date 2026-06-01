a92f21d050c6cd424f90b3eef3cfe2c352e070f2
Luka <luka.matshebelele@gmail.com>
2026-06-01 18:58:24 +0200

    add icons for site info

--- frontend/src/components/site_card/SiteCard.tsx
+++ frontend/src/components/site_card/SiteCard.tsx
@@ -1,3 +1,5 @@
+import { Globe } from "lucide-react"
+import { SmilePlus } from "lucide-react"
 
 interface Props {
     name: string;
@@ -11,8 +13,8 @@ function SiteCard({ name, domain, niche }: Props) {
             <h2 className="text-lg font-semibold">{name}</h2>
 
             {/* use icons */}
-            <p className="text-md text-left">Domain (use icon instead): <span className="text-gray-500 font-semibold"><a href={domain} target="_blank">{domain}</a></span></p>
-            <p className="text-md text-left">Niche (use icon instead): <span className="text-gray-500 font-semibold">{niche}</span></p>
+            <p className="text-md text-left"> <Globe size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold hover:text-blue-500"><a href={domain} target="_blank">{domain}</a></span></p>
+            <p className="text-md text-left"><SmilePlus size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold">{niche}</span></p>
         </div>
     )
 }
