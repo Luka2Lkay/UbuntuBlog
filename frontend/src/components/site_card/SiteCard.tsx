725cd06746f051020b803edeac8649c16c7007cb
Luka <luka.matshebelele@gmail.com>
2026-06-01 19:01:17 +0200

    remove comment

--- frontend/src/components/site_card/SiteCard.tsx
+++ frontend/src/components/site_card/SiteCard.tsx
@@ -11,8 +11,7 @@ function SiteCard({ name, domain, niche }: Props) {
     return (
         <div className="bg-white border border-gray-200 rounded-lg p-4 w-1/2">
             <h2 className="text-lg font-semibold">{name}</h2>
-
-            {/* use icons */}
+            
             <p className="text-md text-left"> <Globe size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold hover:text-blue-500"><a href={domain} target="_blank">{domain}</a></span></p>
             <p className="text-md text-left"><SmilePlus size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold">{niche}</span></p>
         </div>
