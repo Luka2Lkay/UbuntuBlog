31ab12b9ca78f814c0d04affef56b81502c889e1
Luka <luka.matshebelele@gmail.com>
2026-06-03 13:48:15 +0200

    add https to the domain

--- frontend/src/components/site_card/SiteCard.tsx
+++ frontend/src/components/site_card/SiteCard.tsx
@@ -12,7 +12,7 @@ function SiteCard({ name, domain, niche }: Props) {
         <div className="bg-white border border-gray-200 rounded-lg p-4 w-1/2">
             <h2 className="text-lg font-semibold">{name}</h2>
             
-            <p className="text-md text-left"> <Globe size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold hover:text-blue-500"><a href={domain} target="_blank">{domain}</a></span></p>
+            <p className="text-md text-left"> <Globe size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold hover:text-blue-500"><a href={`https://${domain}`} target="_blank" rel="noopener noreferrer">{domain}</a></span></p>
             <p className="text-md text-left"><SmilePlus size={18} className="inline-block mr-2 text-black" />: <span className="text-gray-500 font-semibold">{niche}</span></p>
         </div>
     )
