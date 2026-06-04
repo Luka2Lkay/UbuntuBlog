0696b7f414abf7a2291fabddebcf2b2051b8e08d
Luka <luka.matshebelele@gmail.com>
2026-06-04 09:41:11 +0200

    add router for editing a site

--- backend/src/routes/site_routes.js
+++ backend/src/routes/site_routes.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { createSite, getSites, deleteSite } = require("../controllers/site_controller");
+const {
+  createSite,
+  getSites,
+  deleteSite,
+  editSite,
+} = require("../controllers/site_controller");
 const { validateSiteCreation } = require("../middleware/validation");
 
 const siteRoutes = (app) => {
   const router = express.Router();
 
   router.post("/sites", validateSiteCreation, createSite);
-  router.get("/sites", getSites)
+  router.get("/sites", getSites);
+  router.put("/sites/:siteId", validateSiteCreation, editSite);
   router.delete("/sites/:siteId", deleteSite);
 
   app.use("/api", router);
