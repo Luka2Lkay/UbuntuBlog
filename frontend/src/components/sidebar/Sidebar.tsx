daf51a7082cf011ea0ac5a7339e8f7acbb3c118d
Luka <luka.matshebelele@gmail.com>
2026-06-07 16:27:57 +0200

    add type attribute to the buttons

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -98,7 +98,7 @@ function Sidebar() {
                     {
                         sites.map(clientSite => (
                             <div key={clientSite._id}>
-                                <button onClick={() => setSelectedSite(clientSite)}
+                                <button type="button" onClick={() => setSelectedSite(clientSite)}
                                     className={`text-left px-3 py-2 rounded-md w-full text-sm transition ${selectedSite?._id === clientSite?._id ? 'bg-blue-600 text-white' : 'text-gray-400 hover:bg-gray-800 hover:text-white'}`}
                                 >
                                     <div className="flex items-center justify-between">
@@ -114,13 +114,13 @@ function Sidebar() {
 
                                     <div className="absolute right-120 top-50 z-50 gap-10 w-50 rounded-lg bg-gray-900 border border-gray-800 overflow-hidden shadow-xl">
                                         <div className="border-b border-gray-500 flex justify-end">
-                                            <button className="cursor-pointer p-4 font-semibold hover:text-red-500" onClick={() => setClientMenuOpen(null)}>X</button>
+                                            <button type="button" className="cursor-pointer p-4 font-semibold hover:text-red-500" onClick={() => setClientMenuOpen(null)}>X</button>
                                         </div>
-                                        <button className="w-full gap-2 flex items-center px-4 py-3 text-sm hover:bg-gray-800" onClick={() => navigateToEdit(clientSite?._id)}>
+                                        <button type="button" className="w-full gap-2 flex items-center px-4 py-3 text-sm hover:bg-gray-800" onClick={() => navigateToEdit(clientSite?._id)}>
                                             <Pencil size={16} />
                                             Edit Client
                                         </button>
-                                        <button className="w-full flex items-center gap-2 px-4 py-3 text-sm text-red-400 hover:bg-gray-800" onClick={() => handleDeleteSite(clientSite?._id)}>
+                                        <button type="button" className="w-full flex items-center gap-2 px-4 py-3 text-sm text-red-400 hover:bg-gray-800" onClick={() => handleDeleteSite(clientSite?._id)}>
                                             <Trash size={16} />
                                             Remove Client
                                         </button>
