dfc2e27e357f4faa767c771b2aa6fbde6ab57af4
Luka <luka.matshebelele@gmail.com>
2026-06-07 18:55:05 +0200

    remove redundant icons

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -2,12 +2,13 @@ import { useSiteContext } from "../../context/SiteContext";
 import { type Site } from "../../interfaces/interface";
 import { useState, useEffect, useRef } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { LayoutDashboard, FileText, PlusSquare, MoveRight, MoveLeft, Plus, MoreVertical, Pencil, Trash } from "lucide-react";
+import { LayoutDashboard, FileText, PlusSquare, MoveRight, MoveLeft, Plus } from "lucide-react";
 import { selectSites } from "../../redux/reducers/site_slice";
 import { fetchSitesThunk } from "../../redux/thunks/site_thunk";
 import { useAuth } from "@clerk/react";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux_hooks";
 import { deleteSiteThunk } from "../../redux/thunks/site_thunk";
+
 
 function Sidebar() {
     const { selectedSite, setSelectedSite } = useSiteContext();
