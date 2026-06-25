2a0884d3599928d0740a0814cf31eedf56f17cb1
Luka <luka.matshebelele@gmail.com>
2026-06-25 17:45:19 +0200

    remove logs

--- frontend/src/pages/create_site/CreateSite.tsx
+++ frontend/src/pages/create_site/CreateSite.tsx
@@ -24,27 +24,21 @@ function CreateSite() {
 
         try {
             const token = await getToken({ template: "backend" });
-
-            console.log("post data: ", data)
-            const dispatchResult = await dispatch(postSitesThunk({ siteData: data, token }));
+            const dispatchResult = await dispatch(postSitesThunk({ siteData: data, token })).unwrap();
             const createdSite = dispatchResult.payload as Site;
 
-            console.log("created site: ", createdSite)
-
-            if (createdSite) {
-                setSelectedSite(createdSite);
-                // navigate("/dashboard");
-            }
+            setSelectedSite(createdSite);
+            navigate("/dashboard");
 
         } catch (error) {
-            console.error("Error creating the site", error as string);
+            console.error("Error creating the site", error);
             throw new Error("Failed to create the site");
         }
     }
 
     return (
         <div>
-            <p>{error}</p>
+            <p className="text-red-500 font-semibold">{error}</p>
             <SiteForm onSubmit={handleCreateSite} loading={loading} />
         </div>
     )
