ec398f2e7a3e0a6294ec6b642b09cbf525070b1e
Luka <luka.matshebelele@gmail.com>
2026-08-05 22:49:57 +0200

    new post reducer

--- frontend/src/state/redux/reducers/post_slice.ts
+++ frontend/src/state/redux/reducers/post_slice.ts
@@ -1,4 +1,5 @@
 import { type Post } from "@/interfaces/Post";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface PostState {
   currentPost: Post | null;
@@ -29,3 +30,24 @@ const initialState: PostState = {
   error: null,
   loading: false,
 };
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    setCurrentPost(state, action) {
+      state.currentPost = action.payload;
+    },
+    addPost(state, action) {
+      const newPost = action.payload ?? null;
+
+      const index = state.posts.findIndex((post) => post._id === newPost._id);
+
+      if (index === -1) {
+        state.posts.unshift(newPost);
+      } else {
+        state.posts[index] = newPost;
+      }
+    },
+  },
+});
