27b513429256dca99fd75c4e7d60bfbcd9cc8c64
Luka <luka.matshebelele@gmail.com>
2026-08-09 06:31:03 +0200

    check directories correctly

--- scripts/generate_readme.js
+++ scripts/generate_readme.js
@@ -61,11 +61,11 @@ const generateTree = (directory, prefix = "") => {
     .readdirSync(directory, { withFileTypes: true })
     .filter((entry) => !ignored.has(entry.name))
     .sort((a, b) => {
-      if (a.directory && !b.directory) {
+      if (a.isDirectory() && !b.isDirectory()) {
         return -1;
       }
 
-      if (a.directory && b.directory) {
+      if (a.isDirectory() && b.isDirectory()) {
         return 1;
       }
 
@@ -85,12 +85,11 @@ const generateTree = (directory, prefix = "") => {
         );
       }
 
-      return `${prefix}${connector}${entry.name}/\n`;
+      return `${prefix}${connector}${entry.name}\n`;
     })
     .join("");
 };
 
-console.log("version", frontend.version)
 const version = frontend.version || backend.version || "0.0.0";
 
 const frontendDependencies = {
