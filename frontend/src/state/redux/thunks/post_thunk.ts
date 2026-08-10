3d930b057130ef944a8ad75d5a0b1ef6ee55ecc3
Luka <luka.matshebelele@gmail.com>
2026-08-10 18:54:42 +0200

    update url

--- frontend/src/state/redux/thunks/post_thunk.ts
+++ frontend/src/state/redux/thunks/post_thunk.ts
@@ -9,7 +9,7 @@ import { setCurrentPost } from "@/state/redux/reducers/post_slice";
 import { errorMessages } from "@/helpers/messages_helper";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_LOCAL_URL; // change to live
+const BASE_URL = import.meta.env.VITE_BASE_LIVE_URL;
 
 type NewPost = Omit<Post, "_id">;
 
