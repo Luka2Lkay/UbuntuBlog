d7f5346d2b9337d28fdc7206d3f57cf5b751de0f
Luka <luka.matshebelele@gmail.com>
2026-09-07 15:00:02 +0200

    remove duplication error

--- backend/src/scripts/migrate_site_memberships.js
+++ backend/src/scripts/migrate_site_memberships.js
@@ -8,38 +8,38 @@ const { db } = require("../config/db_config");
 
 async function migrateSiteMemberships() {
   try {
-    mongoose
-      .connect(db.connectionString)
-      .then(() => {
-        console.log("Connected to MongoDB");
-      })
-      .catch((err) => {
-        console.error("Failed to connect to MongoDB", err);
-      });
+    await mongoose.connect(db.connectionString);
+    console.log("Connected to MongoDB");
 
     const sites = await Site.find();
 
     for (const site of sites) {
       const user = await User.findOne({ clerkId: site.userId });
-
-      console.log({ user });
-      if (user) {
-        const siteMember = new SiteMember({
-          siteId: site._id,
-          userId: user._id,
-          role: "admin",
-        });
-        await siteMember.save();
+      if (!user) {
+        console.warn(
+          `User not found for site ${site._id} with clerkId ${site.userId}`,
+        );
+        continue;
       }
+
+      await SiteMember.findOneAndUpdate(
+        { siteId: site._id, userId: user._id },
+        { $setOnInsert: { role: "admin", active: true } },
+        { upsert: true, returnDocument: "after" },
+      );
+
+      console.log(
+        `Migrated site membership for site ${site.name} and user ${user.name}`,
+      );
     }
 
     console.log("Site memberships migration completed successfully.");
-
-    const siteMembers = await SiteMember.find();
-
-    console.log("Site Members:", siteMembers);
   } catch (error) {
     console.error("Error migrating site memberships:", error);
+  } finally {
+    await mongoose.connection.close();
+    console.log("MongoDB connection closed.");
+    process.exit(0);
   }
 }
 
