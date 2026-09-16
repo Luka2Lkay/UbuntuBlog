8a9b8ce0af795934a9a6e81908f44214aebe9424
Luka <luka.matshebelele@gmail.com>
2026-09-16 15:21:58 +0200

    update

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_LIVE_URL;
+export const BASE_URL =
+  import.meta.env.VITE_BASE_LOCAL_URL || import.meta.env.VITE_BASE_LIVE_URL;
 export const redirectUrl =
   import.meta.env.VITE_CLERK_SIGN_IN_FORCE_REDIRECT_URL || "/dashboard";
